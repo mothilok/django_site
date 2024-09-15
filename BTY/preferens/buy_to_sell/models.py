e077221dd7dcfe788f943eb8af37c11ed146fced
moth <motyl9040@gmail.com>
2024-09-15 11:32:09 +0300

    add project

--- BTY/preferens/buy_to_sell/models.py
+++ BTY/preferens/buy_to_sell/models.py
@@ -3,7 +3,6 @@ from django.db import models
 from io import BytesIO
 from PIL import Image
 from django.core.files.base import File
-from pathlib import Path
 
 
 class Poster(models.Model):
@@ -17,55 +16,17 @@ class Poster(models.Model):
     phone_number = models.CharField(max_length=16)
 
     def save(self, *args, **kwargs):
-        print(f'pathhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh {self.miniature}')
         new_image = self.compress_images(self.miniature)
         self.miniature = new_image
         super().save(*args, **kwargs)
 
     def compress_images(self, miniature):
-        print(f'miniat: {miniature}')
-        img = Image.open(miniature)#.convert('RGB')
+        img = Image.open(miniature)
         img.thumbnail((275, 257))
-
-
-
-
         img_io = BytesIO()
-        print(f'img_io: {img_io}')
         img.save(img_io, img.format)
-        print(f'pathhhhhhhhhhhhhhhh miniat_iiiiiiiiiiiiiiiimg: {miniature}')
-        print(f'miniat_typeeeeeeeeeee:' + str(type(miniature)))
         new_image = File(img_io, name=miniature.name)
-
-        print(f'new inaaaaaaaaageeeeeeeee_typeeeeeeeeeee:' + str(type(new_image)))
-        #print(f'newwwwwwwwwwwwwwwwwwwwwwwwwwwwww img: {new_image}')
         return new_image
-
-#
-
-
 
     def __str__(self):
         return self.title
-
-
-
-    #     def save(self, *args, **kwargs):
-    #         super().save(*args, **kwargs)
-    #
-    #     def compress_images(self, miniature):
-    #         img = Image.open(miniature)
-    #         img.thumbnail((275, 257))
-    #         img_io = BytesIO()
-    #         img.save(img_io, optimize=True)
-    #         new_image = File(img_io, name=miniature.name)
-    #         return new_image
-
-
-
-
-    #
-    # def save(self, *args, **kwargs):
-    #     new_image = self.compress_images(self.image)
-    #     self.miniature = new_image
-    #     super().save(*args, **kwargs)
--- BTY/preferens/buy_to_sell/views.py
+++ BTY/preferens/buy_to_sell/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
 from .models import Poster
-from PIL import Image
 
 def home(request):
     request_bd = Poster.objects.order_by('-date_published')[:5]
@@ -9,12 +8,8 @@ def home(request):
     }
     return render(request, "buy_to_sell/home.html", context)
 
-
 def post(request, post_id):
     request_bd = Poster.objects.get(pk=post_id)
-    print(f'путьььььььььььььььььььььььььььььььь: {request_bd.image.path}')
-    print(request_bd.miniature)
-
     context = {
         'data':request_bd
     }
@@ -24,47 +19,12 @@ def post(request, post_id):
 def create_poster(request):
     request_post = request.POST
     if request.method == "POST":
-        print(f'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa: {type(request.FILES["create_image"])}')
         create_poster = Poster()
         create_poster.title = request_post['create_title']
         create_poster.description = request_post['create_description']
         create_poster.price = request_post['create_price']
-        print(f"request image{request.FILES['create_image']}")
         create_poster.image = request.FILES['create_image']
         create_poster.phone_number = request_post['create_number']
-        print(f'last crea_numb{create_poster.phone_number}')
-
         create_poster.miniature = request.FILES['create_image']
-        print(f'kast creat_miniat{type(request.FILES["create_image"])}')
-        print(f'kast creat_miniat{type(create_poster.miniature)}')
-
-        print(f"reque.FILES{request.FILES}")
-        print(f"reque.FILES[image]{request.FILES['create_image']}")
-
-        # image = Image.open(create_poster.image)
-        # image.show()
-        print(f'creaaaaaaaaaaate alllllllllllllllllllll: {create_poster}')
         create_poster.save()
-
-
-
-
-# что такое Meta, как декодировать файл из байтов
-# я кажется понял в чем дело когда происходит сохранение метод save сначало проходится по  image потом по miniature уже по измененному изобр
-# проблема так же остается в типах мне нужно чтобвъы img было типа путь байты или объект файла я не помню где это видел
-
     return render(request, 'buy_to_sell/create_poster.html')
-
-
-
-
-
-    #
-    # if request.method == "POST":
-    #     print(request_post['create_image'])
-    #     if request_post['create_image']:
-    #         resize = Image.open(request_post['create_image'])
-    #         if resize.size[0] > 420 and resize.size[1] > 420:
-    #             resize = resize.resize((resize.width // 2, resize.height // 2))
-    #             request_post['create_image'] = resize
-    #         resize.show()
